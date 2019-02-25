2e901d0654a34ea3c7cf2a9ddd3c1fe4e5b03c6e
visualstudio <visualstudio@230-PC38>
2019-02-25 15:03:55 -0500

    For and while loop

--- C0742332-week7/js/main.js
+++ C0742332-week7/js/main.js
@@ -1,1 +1,16 @@
 ﻿// Your code here!
+
+
+    function p1() {
+        sum = 0;
+
+    for (i = 117; i >= 11; i -= 8) {
+        sum += i;
+
+    }
+
+    return sum;
+}
+
+        document.write("<p> Sum of numbers:" + " " + p1());
+    
